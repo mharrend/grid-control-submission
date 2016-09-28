f8e8b80c4baf04e6f33afdb12fc0a4f598ade39e
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-09-28 14:35:42 +0200

    Update automaticJobCreation.sh

--- automaticJobCreation.sh
+++ automaticJobCreation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Give full dataset name, then short sample name"
-echo "E.g. ./automaticJobcreation.sh /ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8"
+echo "E.g. ./automaticJobCreation.sh /ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8/RunIISpring16MiniAODv2-premix_withHLT_80X_mcRun2_asymptotic_v14-v1/MINIAODSIM ttHTobb_M125_TuneCUETP8M2_ttHtranche3_13TeV-powheg-pythia8"
 export DATASETNAME=$1
 export SAMPLENAME=$2
 
